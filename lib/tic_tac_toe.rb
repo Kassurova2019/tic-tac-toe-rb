1f2d550f66467a31719d1c36cf320fed864971d7
Luba Kassurova <kassurova@icloud.com>
2022-01-26 19:05:57 -0500

    commit tree f721dbeda3445ca4b4bd3627d85eea2e56fe09ce

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,103 @@
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
+def won?(board)
+  WIN_COMBINATIONS.detect do |combination|
+    win_index_1 = combination[0]
+    win_index_2 = combination[1]
+    win_index_3 = combination[2]
+
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
+    if position_1 == position_2 && position_2 == position_3 && position_taken?(board,win_index_1)
+      combination
+    else 
+      false
+    end
+  end
+end
+
+def display_board(board)
+    puts " #{board[0]} | #{board[1]} | #{board[2]} "
+    puts "-----------"
+    puts " #{board[3]} | #{board[4]} | #{board[5]} "
+    puts "-----------"
+    puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+  
+def input_to_index(user_input)
+    user_input.to_i - 1
+end
+
+def move(board, index, current_player)
+    board[index] = current_player
+end
+
+def position_taken?(board, location)
+    board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, index)
+    index.between?(0,8) && !position_taken?(board, index)
+end
+
+  
+  
+
+def turn_count(board)
+ board.count do |token|
+    token == "X" || token == "O"
+ end 
+end 
+   
+
+def current_player(board)
+    turn_count(board) % 2 == 0? "X" : "O"
+end
+ 
+
+def turn(board)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(board, index)
+      move(board, index, current_player(board))
+      display_board(board)
+    else
+      turn(board)
+    end
+end
+
+def full?(board)
+    board.all? do |position|
+      position == "X" || position == "O"
+     end
+end
+
+    def draw?(board)
+        if full?(board) && !won?(board)
+          true
+        else false
+        end
+      end
+def over?(board)
+         
+      won?(board) || draw?(board)
+
+end
+
+
+def winner(board)
+    winner = won?(board) 
+    if winner
+      board[winner.first]
+    end
+end
+
+def play(board)
+       turn(board) until over?(board)
+       if won?(board)
+        puts "Congratulations #{winner(board)}!"
+       elsif draw?(board)
+        puts "Cat's Game!"
+       end
+end
